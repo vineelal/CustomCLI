7a3807d6837632bf6a21c9f091d04398e9731498
vineelal <vineelal@vineela-l.(none)>
2013-11-26 18:14:48 +0530

    Changed packing

---
+++ src/main/java/com/imaginea/jgit/Clone.java
@@ -0,0 +1,66 @@
+package com.imaginea.jgit;
+
+import java.io.File;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.eclipse.jgit.api.CloneCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.TransportException;
+
+public class Clone {
+	 String uri_regexp = "(git|ssh|ftps?|https?).\\/.*\\.git";
+	 String clone_regexp = "git\\sclone";
+	 String directoryNotPresent_regexp = "\\.git$";
+	 String directory_regexp = "\\.git\\s.*";
+	 
+	 Git git = null;
+	 CloneCommand cc = Git.cloneRepository();
+	 Pattern pattern = null;
+	 Matcher matcher = null;
+	 
+	 public void setURI(String command){
+		 pattern = Pattern.compile(uri_regexp);
+         matcher = pattern.matcher(command);
+         if(matcher.find()) {
+        	 System.out.println("setURI "+matcher.group());
+        	 cc.setURI(matcher.group());
+         }
+	 }
+	 
+	 public void setDirectory(String command){
+		 pattern = Pattern.compile(directoryNotPresent_regexp);
+         matcher = pattern.matcher(command);
+         if(!matcher.find()){
+        	 pattern = Pattern.compile(directory_regexp);
+             matcher = pattern.matcher(command);
+             if(matcher.find()){
+            	 File directory = new File(matcher.group().substring(5));
+            	 System.out.println("directory "+directory);
+            	 cc.setDirectory(directory);
+             }
+         }
+	 }
+	 
+	 public void setOptions(String command){
+		 setURI(command);
+		 setDirectory(command);
+	 }
+	 
+	 public String clone(String command){
+		 setOptions(command);
+			try {
+				git = cc.call();
+				System.out.println("successfully clones");
+			} catch (InvalidRemoteException e) {
+				e.printStackTrace();
+			} catch (TransportException e) {
+				e.printStackTrace();
+			} catch (GitAPIException e) {
+				e.printStackTrace();
+			}
+			return "Cloned.....................";
+	 }
+}
